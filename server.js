cc3bc7c43ca1365ffac02074059cce46469196f4
shaleen <mundrashaleen@gmail.com>
2017-06-13 21:46:44 +0530

    added EJS

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({secret: 'hey there!',
+                saveUninialized: true,
+                resave: true}));
+app.set('view engine', 'ejs');
+
+
+/*app.use('/', function(req,res){
+ res.send('our first express program');
+ console.log(req.cookies);
+ console.log('***********');
+ console.log(req.session);
+ });*/
+
+require('./app/routes.js')(app);
+app.listen(port);
+console.log('server running on port: ', port);
